75ac2f542f4e13e0ce50d28a0b8677f2a2976b99
mirzakhalov03 <javohirmirzakhalov@gmail.com>
2025-04-16 12:01:01 +0500

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import ScrollToTop from './components/scrollToTop/ScrollToTop'
 import 'react-toastify/dist/ReactToastify.css';
 import RouteController from './routes'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
